d36969959e9095074e9e870e10fcd783c95bb703
JosueZelay <106643492+JosueZelay@users.noreply.github.com>
2023-08-08 11:48:25 -0600

    actualizacion de route category

--- server/src/routes/categoryRoutes.js
+++ server/src/routes/categoryRoutes.js
@@ -3,6 +3,8 @@ import {
   createCategory,
   deleteCategory,
   getCategory,
+  editCategory,
+  getCategoryById
 } from "../controllers/categoryController.js";
 
 const router = Router();
@@ -10,6 +12,8 @@ const router = Router();
 router.get("/", getCategory);
 router.post("/", createCategory);
 
+router.get("/:id", getCategoryById);
 router.delete("/:id", deleteCategory);
+router.patch("/:id", editCategory);
 
 export default router;
